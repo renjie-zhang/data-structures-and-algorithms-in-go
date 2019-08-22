5c438ca4ddab02cef62220066be2e93c5b74ff07
renjie.zhang <joker_zhang@aliyun.com>
2019-08-22 20:47:41 +0800

    add LinkedQueue

---
+++ LinkedQueue/LinkedQueue.go
@@ -0,0 +1,71 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: renjie.zhang
+ * @Date: 2019-08-22 18:17:58
+ * @LastEditTime: 2019-08-22 18:31:20
+ */
+package LinkedQueue
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type Queue struct {
+	rear *Node
+}
+
+//入队操作
+func (this *Queue) EnQueue(i int) {
+	data := &Node{data: i}
+	if this.rear != nil {
+		data.next = this.rear
+	}
+	this.rear = data
+}
+
+//出队操作
+func (this *Queue) DeQueue() (int, bool) {
+	if this.rear == nil {
+		return 0, false
+	}
+	if this.rear.next == nil {
+		i := this.rear.data
+		this.rear = nil
+		return i, true
+	}
+	current := this.rear
+	for {
+		if current.next.next == nil {
+			i := current.next.data
+			current.next = nil
+			return i, true
+		}
+		current = current.next
+	}
+}
+
+func (this *Queue) Peek() (int, bool) {
+	if this.rear == nil {
+		return 0, false
+	}
+	return this.rear.data, true
+}
+
+func (this *Queue) Get() []int {
+	var items []int
+	current := this.rear
+	for current != nil {
+		items = append(items, current.data)
+		current = current.next
+	}
+	return items
+}
+func (this *Queue) IsEmpty() bool {
+	return this.rear == nil
+}
+
+func(this *Queue) Empty(){
+	this.rear = nil
+}
---
+++ LinkedQueue/LinkedQueue_test.go
@@ -0,0 +1,22 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: renjie.zhang
+ * @Date: 2019-08-22 18:32:16
+ * @LastEditTime: 2019-08-22 19:34:44
+ */
+package LinkedQueue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLinkedQueue(t *testing.T) {
+	N1 := Node{data: 1}
+	Q := Queue{rear: &N1}
+	Q.EnQueue(2)
+	var el []int
+	el = Q.Get()
+	fmt.Println(el)
+}
