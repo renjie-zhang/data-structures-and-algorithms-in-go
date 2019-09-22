1d5655fb7e603a6905932df35af47e53b50a62ad
renjie.zhang <joker_zhang@aliyun.com>
2019-09-22 18:55:40 +0800

    update BinaryFind

--- Binaryfind/BinaryFind_test.go
+++ Binaryfind/BinaryFind_test.go
@@ -1,9 +1,10 @@
-package Binaryfind
+package BinaryFind
 
 import (
 	"testing"
 )
 
-
-func TestBinaryFind(test *testing.T){
+func TestBinaryFind(test *testing.T) {
+	var array = [6]int{12, 23, 34, 45, 56, 67}
+	BinaryFind(&array, 0, len(array)-1, 45)
 }
