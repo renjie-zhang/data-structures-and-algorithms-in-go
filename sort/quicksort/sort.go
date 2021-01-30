609243b9960d4fac744c33321b35c5e21333e3ab
renjie zhang <joker_zhang@aliyun.com>
2021-01-30 22:00:27 +0800

    update quick sort

--- sort/quicksort/sort.go
+++ sort/quicksort/sort.go
@@ -9,22 +9,22 @@ func QuickSort(arr []int, low int, high int) {
 	if low > high {
 		return
 	}
-	j := partition(arr, low, high)
+	j := part(arr, low, high)
 	QuickSort(arr, low, j-1)
 	QuickSort(arr, j+1, high)
 }
 
-func partition(arr []int, low int, high int) int {
+func part(arr []int, low int, high int) int {
 	i := low
 	// 使用最右边作为基准
-	temp := arr[high]
-	for j := low;j< high;j++{
-		if arr[j] < temp{
-			arr[i],arr[j] = arr[j],arr[i]
+	pivot := arr[high]
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
 			i++
 		}
 	}
-	arr[i],arr[high] = arr[high],arr[i]
+	arr[i], arr[high] = arr[high], arr[i]
 	return i
 }
 
